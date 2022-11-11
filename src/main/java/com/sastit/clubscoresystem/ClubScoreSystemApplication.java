5298d407a8b95616bdcfc0fe122a984fd9d9190c
Taoning Ge <gtn1024o@outlook.com>
2022-11-11 21:21:06 +0800

    init

---
+++ src/main/java/com/sastit/clubscoresystem/ClubScoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sastit.clubscoresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubScoreSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubScoreSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sastit/clubscoresystem/ClubScoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sastit.clubscoresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubScoreSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
