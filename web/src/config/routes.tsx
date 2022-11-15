67f9cb946811dc2f49a133dd6ed318dd00ea637a
Taoning Ge <gtn1024o@outlook.com>
2022-11-15 09:17:40 +0800

    异步加载组件

--- web/src/config/routes.tsx
+++ web/src/config/routes.tsx
@@ -1,16 +1,14 @@
 import { RouteRecordRaw } from "vue-router";
-import { Login } from "../views/login/Login";
-import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "home",
-    component: Welcome,
+    component: () => import("../views/Welcome"),
   },
   {
     path: "/login",
     name: "login",
-    component: Login,
+    component: () => import("../views/login/Login"),
   },
 ];
