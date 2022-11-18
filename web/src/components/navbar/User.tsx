89b61f8d31df2218789bcd24e392d398cd5c1c80
Taoning Ge <gtn1024o@outlook.com>
2022-11-18 22:28:44 +0800

    支持用户注销登录

--- web/src/components/navbar/User.tsx
+++ web/src/components/navbar/User.tsx
@@ -2,18 +2,33 @@ import { defineComponent } from "vue";
 import style from "./User.module.scss";
 import { NButton } from "naive-ui";
 import { useRouter } from "vue-router";
+import { useUserStore } from "../../store/user";
 
 export const User = defineComponent({
   setup() {
     const router = useRouter();
+    const userStore = useUserStore();
     const loginBtnOnClick = () => {
-      router.push("/login");
+      router.push({ name: "login" });
+    };
+    const logoutBtnOnClick = () => {
+      userStore.logout();
+      router.push({ name: "login" });
     };
     return () => (
       <div class={style.user}>
-        <NButton text onClick={loginBtnOnClick}>
-          登录
-        </NButton>
+        {userStore.id ? (
+          <>
+            <span>{userStore.username}</span>
+            <NButton class={style.btn} text onClick={logoutBtnOnClick}>
+              注销
+            </NButton>
+          </>
+        ) : (
+          <NButton text onClick={loginBtnOnClick}>
+            登录
+          </NButton>
+        )}
       </div>
     );
   },
