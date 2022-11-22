d67d566598966c2fae5ecbc67110d0c8b85acd8c
Taoning Ge <gtn1024o@outlook.com>
2022-11-22 08:38:56 +0800

    更新创建时间和修改时间字段

--- src/main/java/com/sastit/clubscoresystem/model/entity/BaseEntity.java
+++ src/main/java/com/sastit/clubscoresystem/model/entity/BaseEntity.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,9 +21,11 @@ public abstract class BaseEntity {
   private Long id;
 
   @CreationTimestamp
+  @Column(nullable = false, updatable = false)
   private Instant createdAt;
 
   @UpdateTimestamp
+  @Column(nullable = false)
   private Instant updatedAt;
 
   private Boolean deleted = false;
